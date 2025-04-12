283daf654a2b56de6570a5057df7c4236e72c125
Jailani Abdul Rahman <jailani.har@googlemail.com>
2025-04-12 09:02:25 +0800

    Rolling back to savepoint

--- bankdb.sql
+++ bankdb.sql
@@ -11,3 +11,41 @@ CREATE TABLE accounts (
 INSERT INTO accounts VALUES
 (1, 'Abu', 1000.00),
 (2, 'Bakar', 1000.00);
+
+SELECT * from accounts;
+-- withdraw
+UPDATE accounts SET balance = balance - 200 WHERE id=1;
+
+SELECT * from accounts;
+
+-- deposit
+UPDATE accounts SET balance = balance + 200 WHERE id=2;
+
+SELECT * from accounts;
+
+START TRANSACTION;
+UPDATE accounts SET balance = balance - 200 WHERE id=1;
+SELECT * FROM accounts;
+UPDATE accounts SET balance = balance + 200 WHERE id=2;
+SELECT * FROM accounts;
+COMMIT;
+
+START TRANSACTION;
+UPDATE accounts SET balance = balance - 200 WHERE id=1;
+SELECT * FROM accounts;
+UPDATE accounts SET balance = balance + 200 WHERE id=99;
+SELECT * FROM accounts;
+ROLLBACK;
+
+SELECT * FROM accounts;
+
+START TRANSACTION;
+UPDATE accounts SET balance = balance - 200 WHERE id=1;
+SAVEPOINT withdraw_money;
+UPDATE accounts SET balance = balance + 200 WHERE id=2;
+SAVEPOINT deposit_money;
+SELECT * FROM accounts;
+ROLLBACK TO SAVEPOINT withdraw_money;
+SELECT * FROM accounts;
+ROLLBACK;
+
